cc68ad7e9315bfeb73b6ac4be8a1705a296027e4
user <user@host.docker.internal>
2022-11-26 19:35:23 +0200

    Properties

--- src/main/java/telran/java2022/person/service/PersonServiceImpl.java
+++ src/main/java/telran/java2022/person/service/PersonServiceImpl.java
@@ -3,11 +3,14 @@ package telran.java2022.person.service;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import telran.java2022.person.dao.PersonRepository;
@@ -24,7 +27,11 @@ public class PersonServiceImpl implements PersonService {
 	final ModelMapper modelMapper;
 
 	@Override
+	@Transactional
 	public Boolean addPerson(PersonDto personDto) {
+		if(personRepository.existsById(personDto.getId())) {
+			return false;
+		}
 		personRepository.save(modelMapper.map(personDto, Person.class));
 		return true;
 	}
@@ -36,6 +43,7 @@ public class PersonServiceImpl implements PersonService {
 	}
 
 	@Override
+	@Transactional
 	public PersonDto removePerson(Integer id) {
 		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundExeption::new);
 		personRepository.deleteById(id);
@@ -43,54 +51,58 @@ public class PersonServiceImpl implements PersonService {
 	}
 
 	@Override
+	@Transactional
 	public PersonDto updatePersonName(Integer id, String name) {
 		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundExeption::new);
 		person.setName(name);
-		personRepository.save(person);
 		return modelMapper.map(person, PersonDto.class);
 	}
 
 	@Override
+	@Transactional
 	public PersonDto updatePersonAddress(Integer id, AddressDto addressDto) {
 		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundExeption::new);
 		person.getAddress().setBuilding(addressDto.getBuilding());
 		person.getAddress().setCity(addressDto.getCity());
 		person.getAddress().setStreet(addressDto.getStreet());
-		personRepository.save(person);
 		return modelMapper.map(person, PersonDto.class);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<PersonDto> findPersonsByCity(String city) {
-		return getAllPersonsStream().filter(n->n.getAddress().getCity().equals(city))
-									.map(n->modelMapper.map(n, PersonDto.class))
-									.collect(Collectors.toList());
+		return personRepository.findByAddressCity(city)
+				.map(p -> modelMapper.map(p, PersonDto.class))
+				.collect(Collectors.toList());
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<PersonDto> findPersosnsByName(String name) {
-		return getAllPersonsStream().filter(n->n.getName().equals(name))
-				.map(n->modelMapper.map(n, PersonDto.class))
+		return personRepository.findByName(name)
+				.map(p -> modelMapper.map(p, PersonDto.class))
 				.collect(Collectors.toList());
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<PersonDto> findPersonsBetweenAge(Integer minAge, Integer maxAge) {
-		return getAllPersonsStream().filter(n->LocalDate.now().minus(maxAge, ChronoUnit.YEARS).isBefore(n.getBirthDate())
-				&&LocalDate.now().minus(minAge, ChronoUnit.YEARS).isAfter(n.getBirthDate()))
-				.map(n->modelMapper.map(n, PersonDto.class))
+		LocalDate from = LocalDate.now().minusYears(maxAge);
+		LocalDate to = LocalDate.now().minusYears(minAge);
+		return personRepository.findByBirthDateBetween(from, to)
+				.map(p -> modelMapper.map(p, PersonDto.class))
 				.collect(Collectors.toList());
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<CityPopulationDto> getCitiesPopulation() {
-		// TODO Auto-generated method stub
-		return null;
+//		Map<String, Long> population = StreamSupport.stream(personRepository.findAll().spliterator(), false)
+//				.collect(Collectors.groupingBy(p -> p.getAddress().getCity(), Collectors.counting()));
+//		return population.entrySet().stream()
+//				.map(e -> new CityPopulationDto(e.getKey(), e.getValue()))
+//				.collect(Collectors.toList());
+		return personRepository.getCitiesPopulation();
 	}
 	
-	private Stream<Person> getAllPersonsStream() {
-		List<Person>list = (List<Person>) personRepository.findAll();
-		return list.stream();
-	}
-
 }
